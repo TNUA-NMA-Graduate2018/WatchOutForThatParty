866a58600bd38a0b77e5a94d30d8e03a3b0b3be9
aprilcoffee <8765498@gmail.com>
2018-05-04 17:54:46 +0800

    update

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var ellipseSize = 0;
 var isLoaded = false;
 var glitch;
 var imgSrc = 'per_imgs/taipei.jpg';
+var msgImg;
+
 var start = false;
 var mode = 0;
 var scheduleMode = 0;
@@ -18,6 +20,8 @@ var img5;
 var img6;
 var img7;
 
+var colorRed,colorGreen,colorBlue;
+
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
 
@@ -28,6 +32,9 @@ function setup() {
   //canvas.style('display','block');
   background(51, 0);
   bgimg = loadImage("schedule/imgs/bg.jpg");
+
+  msgImg = loadImage("per_imgs/msg.jpg");
+
   img0 = loadImage("schedule/imgs/00opentime.png");
   img1 = loadImage("schedule/imgs/01teacher.png");
   img2 = loadImage("schedule/imgs/02talk.png");
@@ -37,7 +44,7 @@ function setup() {
   img6 = loadImage("schedule/imgs/06DJTaro.png");
   img7 = loadImage("schedule/imgs/07DJKlone.png");
 
-  socket = io.connect('10.17.0.10:3000');
+  socket = io.connect('127.0.0.1:3000');
     //please change the ip to server ip
   socket.on('schedule', newDrawing);
   socket.on('mouse', newDrawingLTG);
@@ -65,20 +72,31 @@ function newDrawingLTG(data) {
 
   console.log(data);
   ellipseSize = data.x;
-
-  if (data.x == -3) { //reset windows
+  if(data.x === 10){
+    mode = -2;
+  }
+  if(data.x === 0){
+    mode = -1;
+  }
+  else if (data.x === -3) { //reset windows
     mode = 0;
     start = false;
     isloaded = false;
     background(100,255,100,0);
-  } else if (data.x == -2) {
+  } else if (data.x === -2) {
     start = true;
     isloaded = false;
     mode = 2;
-  } else if (data.x == -1) {
+  } else if (data.x === -1) {
     isLoaded = true;
     mode = 1;
+  } else if(data.x === -4){
+    mode = 3;
+    colorRed = data.R;
+    colorGreen = data.G;
+    colorBlue = data.B;
   }
+
 }
 function newDrawing(data) {
   mode = 0;
@@ -91,33 +109,42 @@ function newDrawing(data) {
   scheduleMode = data.s;
   switch (scheduleMode) {
     case 0:
-      image(img0, width/2 - 250, 175,500,150);
+      image(img0, width/2 - 183, 140,366,150);
       break;
     case 1:
-      image(img1, width/2 - 250, 175,500,150);
+      image(img1, width/2 - 175, 140,350,150);
       break;
     case 2:
-      image(img2, width/2 - 250, 175,500,150);
+      image(img2, width/2 - 183, 140,366,150);
       break;
     case 3:
-      image(img3, width/2 - 250, 175,500,150);
+      image(img3, width/2 - 156, 140,312,176);
       break;
     case 4:
-      image(img4, width/2 - 250, 175,500,150);
+      image(img4, width/2 - 316, 140,632,150);
       break;
     case 5:
-      image(img5, width/2 - 250, 175,500,150);
+      image(img5, width/2 - 580, 140,1161,175);
       break;
     case 6:
-      image(img6, width/2 - 250, 175,500,150);
+      image(img6, width/2 - 192, 140,384,150);
       break;
     case 7:
-      image(img7, width/2 - 250, 175,500,150);
+      image(img7, width/2 - 224, 140,448,150);
       break;
   }
 }
 function draw() {
-  if (mode === 0) {
+  if(mode === -2){
+    background(0, 0);
+    image(msgImg,width/2 - 175,height/2-130,175,45);
+  }
+  else if(mode === -1){
+
+      background(0, 0);
+      image(msgImg,random(0,width-175),random(0,height-45),175,45);
+  }
+  else if (mode === 0) {
     background(0, 0);
   }else if(mode === 1){
     background(0);
@@ -130,6 +157,11 @@ function draw() {
   else if(mode === 2){
     ellipseSize -= 30;
     background(ellipseSize);
+  }
+  else if(mode ===3){
+    ellipseSize -= 30;
+    var ellipseSizeTemp = map(ellipseSize,0,255,0,1);
+    background(colorRed*ellipseSizeTemp,colorGreen*ellipseSizeTemp,colorBlue*ellipseSizeTemp);
   }
 }
 class Glitch {
